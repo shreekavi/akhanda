51161154ecc04366f7d049b7fa5118fd81341b05
Shree Dittakavi <shreekavi@gmail.com>
2009-03-23 15:49:01 +0530

    Fixed the routes for login and logout, Created a dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller =>'session', :action=>'new'
+  map.resources :users
+  map.resource :session
+  map.root :controller =>'sessions', :action=>'new'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
-
-  map.resource :session
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
 
